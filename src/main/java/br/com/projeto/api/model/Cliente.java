afcb077995b317bd9b93e7057e594cf9591456a7
luiz.junior <luiz.junior@neomind.com.br>
2024-01-10 14:41:18 -0300

    Configurado a entidade(model)

--- src/main/java/br/com/projeto/api/model/Cliente.java
+++ src/main/java/br/com/projeto/api/model/Cliente.java
@@ -1,4 +1,16 @@
 package br.com.projeto.api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "clientes")
+@Data
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private int idade;
+    private String cidade;
 }
