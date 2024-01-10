471a317aeec7ad47d911cc00052228524c16e860
luiz.junior <luiz.junior@neomind.com.br>
2024-01-10 14:54:44 -0300

    Criando rota para cadastrar cliente e testando no Postman.

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,8 +1,11 @@
 package br.com.projeto.api.controller;
 
+import br.com.projeto.api.model.Cliente;
 import br.com.projeto.api.repository.Repositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /*Para Controller de rest */
@@ -13,8 +16,16 @@ public class Controller {
     * Porem ele é gerenciado pelo Spring e então ele só instancia quando necessario. */
     @Autowired
     private Repositorio acao;
+
+    @PostMapping("/cadastrar")
+    /*RequestBody significa que eu preciso de um body do tipo cliente.*/
+    public Cliente cadastrar(@RequestBody Cliente c){
+        return acao.save(c);
+    }
     @GetMapping("/")
     public String teste(){
         return "Hello World!";
     }
+
+
 }
