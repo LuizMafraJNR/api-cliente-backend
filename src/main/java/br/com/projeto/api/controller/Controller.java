9b79967131def09ff6005c612e8eafa9de5a3bcd
luiz.junior <luiz.junior@neomind.com.br>
2024-01-10 14:35:47 -0300

    Configurando a controller para primeiro acesso na web e aparecer o hello world. Ajuste na conexão com o banco.

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,4 +1,13 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*Para Controller de rest */
+@RestController
 public class Controller {
+    @GetMapping("/")
+    public String teste(){
+        return "Hello World!";
+    }
 }
