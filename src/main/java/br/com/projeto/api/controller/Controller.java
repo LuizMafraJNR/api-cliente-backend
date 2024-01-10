b15326d0b6da0368784d43f60fe5c4b69d6e050c
luiz.junior <luiz.junior@neomind.com.br>
2024-01-10 14:48:57 -0300

    Annotation @Autowired

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,11 +1,18 @@
 package br.com.projeto.api.controller;
 
+import br.com.projeto.api.repository.Repositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*Para Controller de rest */
 @RestController
 public class Controller {
+
+    /*Serve para instanciar o nosso repositorio para reaalizar alguma ação dentro do BDA.
+    * Porem ele é gerenciado pelo Spring e então ele só instancia quando necessario. */
+    @Autowired
+    private Repositorio acao;
     @GetMapping("/")
     public String teste(){
         return "Hello World!";
